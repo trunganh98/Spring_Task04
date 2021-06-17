d4ed07397dcec3bb0c2e27e41efc7e715cf47b5e
trunganh98 <60956094+trunganh98@users.noreply.github.com>
2021-06-17 07:44:35 +0700

    Task04

---
+++ Sprin-TestDatabase/src/main/java/com/example/sprintestdatabase/SprinTestDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprintestdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinTestDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprinTestDatabaseApplication.class, args);
+    }
+
+}
---
+++ Sprin-TestDatabase/src/main/java/com/example/sprintestdatabase/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.example.sprintestdatabase.service;
+
+import com.example.sprintestdatabase.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+	
+}
